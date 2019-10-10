a6fe5372a42cf98f931790d29dc7e8ee860c9e30
Jesus121299 <43411498+Jesus121299@users.noreply.github.com>
2019-10-10 13:15:45 -0500

    Funcion abrir archivo

---
+++ js/archivo.js
@@ -0,0 +1,11 @@
+// para abrir el archivo externo
+function processFiles(files) {
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var output = document.getElementById("fileOutput");
+
+        output.textContent = e.target.result;
+    };
+    reader.readAsText(file);
+}
